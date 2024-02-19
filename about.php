eb6e5054717986bf74721cdb32924eaa6536f867
Himal Dahal <51692864+himaldahal@users.noreply.github.com>
2024-02-19 13:26:33 +0545

    Update about.php

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ require('includes/config.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Typing Test</title>
+    <title>Typing Gen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
@@ -14,8 +14,17 @@ require('includes/config.php');
 <?php include 'includes/navbar.php';?>
     <div class="container">
             <h1>Welcome to Typing Test Site</h1>
-            <p>I (Umanga Pokhrel) & Charles lama developed this site for partial fullfillment of NEB XII Examnination.We build the site using HTML,jQuery, Boostrap 5(css libary for responsive UI) & php.We have used SQL db for this project.</p>
-</div>
+            <p>This is a Typing speed test site.Developed by
+                <a href="https://github.com/himaldahal/" class="text-decoration-none fw-bold ">Himal Dahal</a>.This site is built using PHP,Bootstrap & JS.   
+            </p>
+        <h3>Features</h3>
+        <h4>1. Personal Record Keeping:</h4>
+        <p>Typingen allows users to maintain personal typing records, including speed, accuracy, and progress over time. Through detailed analytics and visualizations, users can track their performance, identify areas for improvement, and set personal goals to enhance their typing proficiency.</p>
+        <h4>2. Authentication:</h4>
+        <p>The website offers secure authentication functionality, enabling users to create individual accounts and securely log in to access personalized features and content. With robust user authentication mechanisms in place, Typingen ensures the privacy and security of user data, fostering trust and confidence among its user community.</p>
+        <h4>3. Leaderboard:</h4>
+        <p>Typingen features a dynamic leaderboard that showcases the typing prowess of its users. By ranking users based on factors such as typing speed, accuracy, and completion of challenges, the leaderboard fosters healthy competition and motivation among users. Users can compare their performance with others, strive for higher rankings, and celebrate achievements within the Typingen community.</p>
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
